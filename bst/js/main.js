0e205e55690bbb112f0d22402883f88fafd026c3
kyle <nojaim.k@gmail.com>
2025-04-01 10:41:09 -0400

    thru max,min,insert,contains

--- bst/js/main.js
+++ bst/js/main.js
@@ -16,20 +16,44 @@ class BST{
         return this.count;
     }
 
-    insert(){
+    insert(value){
+        this.count++;
 
+        let newNode = newNode(value)
+
+        const searchTree = node => {
+            if (value < node.value){
+                if (!node.left) node.left = newNode
+                else{searchTree(node.left)}
+            }
+            else if(value > node.value){
+                if (!node.right) node.right = newNode
+                else{searchTree(node.right)}
+            }
+        }
+        searchTree(this.root)
     }
 
     min(){
-
+        let current = this.root
+        while (current.left) current = current.left
+        return current.value
     }
 
     max(){
-
+        let current = this.root
+        while (current.right) current = current.right
+        return current.value
     }
 
-    contains(){
-
+    contains(value){
+        let current = this.root
+        while (current){
+            if (value === current.value) return true
+            if (value < current.value) current = current.left
+            else{current = current.right}
+        }
+        return false;
     }
 
     // depth first search
