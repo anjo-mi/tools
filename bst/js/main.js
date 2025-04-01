6fd24d51e2eb2079a9c9e19a02f3567ad256521e
kyle <nojaim.k@gmail.com>
2025-04-01 11:07:01 -0400

    breadth first searches finished

--- bst/js/main.js
+++ bst/js/main.js
@@ -19,7 +19,7 @@ class BST{
     insert(value){
         this.count++;
 
-        let newNode = newNode(value)
+        let newNode = newNode(value);
 
         const searchTree = node => {
             if (value < node.value){
@@ -31,23 +31,23 @@ class BST{
                 else{searchTree(node.right)}
             }
         }
-        searchTree(this.root)
+        searchTree(this.root);
     }
 
     min(){
-        let current = this.root
-        while (current.left) current = current.left
-        return current.value
+        let current = this.root;
+        while (current.left) current = current.left;
+        return current.value;
     }
 
     max(){
-        let current = this.root
-        while (current.right) current = current.right
-        return current.value
+        let current = this.root;
+        while (current.right) current = current.right;
+        return current.value;
     }
 
     contains(value){
-        let current = this.root
+        let current = this.root;
         while (current){
             if (value === current.value) return true
             if (value < current.value) current = current.left
@@ -65,12 +65,12 @@ class BST{
         const result = [];
 
         const traverse = node => {
-            if (node.left) traverse(node.left)
+            if (node.left) traverse(node.left);
             
-            result.push(node.value)
+            result.push(node.value);
 
-            if (node.right) traverse(node.right)
-        }
+            if (node.right) traverse(node.right);
+        };
         return result;
     }
     
@@ -80,12 +80,12 @@ class BST{
         const result = [];
 
         const traverse = node => {
-            result.push(node.value)
-            if (node.left) traverse(node.left)
-            if (node.right) traverse(node.right)
-        }
+            result.push(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        };
         return result;
-    }
+    };
      
     // post order
     // search left, right, root
@@ -93,10 +93,10 @@ class BST{
         const result = [];
 
         const traverse = node => {
-            if (node.left) traverse(node.left)
-            if (node.right) traverse(node.right)
-            result.push(node.value)
-        }
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            result.push(node.value);
+        };
         return result;
     }
 
@@ -104,6 +104,17 @@ class BST{
     // breadth first seaches level by level, jumps across branches
     // uses a queue
     bfs(){
+        const result = [];
+        let queue = [];
 
+        queue.push(this.root)
+
+        while (queue.length){
+            let current = queue.shift();
+            result.push(current);
+            if (current.left) queue.push(current.left)
+            if (current.right) queue.push(current.right)
+        }
+        return result;
     }
 }
